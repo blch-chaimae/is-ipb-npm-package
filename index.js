171b6de234f1db7e33116bf72c0d2f26c9f3b75c
blch-chaimae <chaimaeblh@gmail.com>
2023-04-19 01:12:11 +0000

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isIPB(msg) {
+  console.log("Hello from IPB node module");
   return msg === "This is IPB node module";
 }
 
 module.exports = isIPB;
+
+exports.printMsg = function () {
+  console.log("Hello, this is a message from IPB node module");
+};
